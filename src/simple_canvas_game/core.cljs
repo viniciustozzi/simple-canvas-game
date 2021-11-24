840f5c09464d86a25c1293e9228dd83330bc6b3a
Vinicius Vieira Tozzi <tozzi@Viniciuss-Mini.fritz.box>
2021-11-24 22:22:48 +0100

    Added collision of paddle to bottomn and top of the screen

--- src/simple_canvas_game/core.cljs
+++ src/simple_canvas_game/core.cljs
@@ -4,20 +4,39 @@
    [reagent.dom :as d]))
 
 (def SCREEN-WIDTH 500)
-(def SCREEN-HEIGHT 500)
-(def PADDLE-SPEED 0.1)
+(def SCREEN-HEIGHT 300)
 
-(def game-state (r/atom {}))
+(def PADDLE-SPEED 1)
+(def PADDLE-WIDTH 20)
+(def PADDLE-HEIGHT 80)
 
-(defn setup [state]
-  (reset! game-state state))
+(def game-state (r/atom
+                 {:paddle1 {:pos {:x 10 :y 100}
+                            :dirY 0}
+                  :paddle2 {:pos {:x (- SCREEN-WIDTH 20) :y 100}
+                            :dirY 0}
+                  :ball {:pos {:x (/ SCREEN-WIDTH 2) :y (/ SCREEN-HEIGHT 2)}
+                         :size 5}
+                  :input {:arrow-up false
+                          :arrow-down false}}))
+
+(defn draw-paddles [state context]
+  (set! (.-fillStyle context) "black")
+  (.fillRect context (-> state :paddle1 :pos :x) (-> state :paddle1 :pos :y) PADDLE-WIDTH PADDLE-HEIGHT)
+  (.fillRect context (-> state :paddle2 :pos :x) (-> state :paddle2 :pos :y) PADDLE-WIDTH PADDLE-HEIGHT))
+
+(defn draw-ball [state context]
+  (set! (.-fillStyle context) "black")
+  (.beginPath context)
+  (.arc context (-> state :ball :pos :x) (-> state :ball :pos :y) (-> state :ball :size) 0 (* 2 3.14))
+  (.stroke context))
 
 (defn draw [state]
   (let [canvas (.getElementById js/document "game-canvas")
         context (.getContext canvas "2d")]
     (.clearRect context 0 0 SCREEN-WIDTH SCREEN-HEIGHT)
-    (set! (.-fillStyle context) "black")
-    (.fillRect context (-> state :paddle1 :pos :x) (-> state :paddle1 :pos :y) 20 150)))
+    (draw-paddles state context)
+    (draw-ball state context)))
 
 (defn on-key-down [event]
   (case (.-code event)
@@ -30,7 +49,7 @@
     "ArrowDown" (swap! game-state assoc-in [:input :arrow-down] false)
     "ArrowUp" (swap! game-state assoc-in [:input :arrow-up] false)))
 
-(defn observe-key-events []
+(defn listen-for-key-events []
   (.addEventListener js/document "keydown" on-key-down false)
   (.addEventListener js/document "keyup" on-key-up false))
 
@@ -44,10 +63,18 @@
                                      arrow-down -1
                                      :else 0)))))
 
+(defn new-paddle-pos [current-posY dir]
+  (let [new-posY (- current-posY (* PADDLE-SPEED dir))]
+    (if (and (< 0 new-posY) (>= SCREEN-HEIGHT (+ new-posY PADDLE-HEIGHT)))
+      new-posY
+      current-posY)))
+
 (defn update-paddle [state]
-  (let [dir1 (-> state :paddle1 :dirY)
+  (let [dir (-> state :paddle1 :dirY)
         posY (-> state :paddle1 :pos :y)]
-    (assoc-in state [:paddle1 :pos :y] (+ PADDLE-SPEED posY))))
+    (-> state
+        (assoc-in [:paddle1 :pos :y] (new-paddle-pos posY dir))
+        (assoc-in [:paddle2 :pos :y] (new-paddle-pos posY dir)))))
 
 (defn update-game [state]
   (swap! game-state #(-> state
@@ -55,16 +82,7 @@
                          update-paddle)))
 
 (defn run-game []
-  (setup {:paddle1 {:pos {:x 10 :y 50}
-                    :dirY 0}
-          :paddle2 {:pos {:x 10 :y 10}
-                    :dirY 0}
-          :ball {:pos {:x (/ SCREEN-WIDTH 2) :y (/ SCREEN-HEIGHT 2)}}
-          :input {:arrow-up false
-                  :arrow-down false}})
-
-  (observe-key-events)
-
+  (listen-for-key-events)
   (js/setInterval (fn []
                     (-> @game-state
                         (update-game)
